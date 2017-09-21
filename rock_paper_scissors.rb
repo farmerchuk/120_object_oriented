8e51a025ddadfdf7f14fe9efb44658b2a4a5a5ee
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-09-21 10:55:12 -0700

    initial commit

---
+++ rock_paper_scissors.rb
@@ -0,0 +1,27 @@
+class Player
+  def initialize
+    # maybe a "name"? what about a "move"?
+  end
+
+  def choose
+
+  end
+end
+
+class Move
+  def initialize
+    # seems like we need something to keep track
+    # of the choice... a move object can be "paper", "rock" or "scissors"
+  end
+end
+
+class Rule
+  def initialize
+    # not sure what the "state" of a rule object should be
+  end
+end
+
+# not sure where "compare" goes yet
+def compare(move1, move2)
+
+end
