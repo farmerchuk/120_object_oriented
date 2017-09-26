07e0f314b0cfa7515f1028b35689193ff0475ab8
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-09-26 07:59:39 -0700

    added methods for detecting the winner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,4 +1,8 @@
 class Board
+  WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
+                  [[1, 5, 9], [3, 5, 7]]              # diagonal
+
   attr_reader :squares
 
   def initialize
@@ -16,13 +20,42 @@ class Board
   def set_square_at(position, marker)
     squares[position].marker = marker
   end
+
+  def empty_square_positions
+    @squares.keys.select { |key| @squares[key].unmarked? }
+  end
+
+  def full?
+    empty_square_positions.empty?
+  end
+
+  def winner?
+    !!detect_winner
+  end
+
+  def detect_winner
+    WINNING_LINES.each do |line|
+      if line.all? { |pos| square_at(pos).marker == TTTGame::HUMAN_MARKER }
+        return TTTGame::HUMAN_MARKER
+      elsif line.all? { |pos| square_at(pos).marker == TTTGame::COMP_MARKER }
+        return TTTGame::COMP_MARKER
+      end
+    end
+    nil
+  end
 end
 
 class Square
+  EMPTY_SQUARE = ' '
+
   attr_accessor :marker
 
-  def initialize(marker = ' ')
+  def initialize(marker = EMPTY_SQUARE)
     @marker = marker
+  end
+
+  def unmarked?
+    marker == EMPTY_SQUARE
   end
 
   def to_s
@@ -60,6 +93,10 @@ class TTTGame
   end
 
   def display_board
+    system 'clear'
+    puts "You're a #{human.marker}"
+    puts "Computer is a #{computer.marker}"
+    puts
     puts "     |     |     "
     puts "  #{board.square_at(1)}  |  #{board.square_at(2)}  |  #{board.square_at(3)}  "
     puts "-----|-----|-----"
@@ -73,33 +110,42 @@ class TTTGame
   def human_moves
     input = nil
     loop do
-      print 'Choose a square between 1 and 9: '
+      print "Choose a square from #{board.empty_square_positions.join(', ')}: "
       input = gets.chomp.to_i
-      break if (1..9).include? input
+      break if board.empty_square_positions.include?(input)
       puts 'Sorry, that is not a valid choice...'
     end
     board.set_square_at(input, human.marker)
   end
 
   def computer_moves
-    input = (1..9).to_a.sample
+    input = board.empty_square_positions.to_a.sample
     board.set_square_at(input, computer.marker)
+  end
+
+  def display_result
+    display_board
+
+    if board.full? && !board.winner?
+      puts "It's a tie!"
+    else
+      puts "The winner is #{board.detect_winner}!"
+    end
   end
 
   def play
     display_welcome_message
+    display_board
     loop do
-      display_board
       human_moves
-      # display_board
-      # break if someone_won? || board_full?
+      break if board.winner? || board.full?
+      display_board
 
       computer_moves
-      # break if someone_won? || board_full?
+      break if board.winner? || board.full?
       display_board
-      break
     end
-    # display_result
+    display_result
     display_goodbye_message
   end
 end
