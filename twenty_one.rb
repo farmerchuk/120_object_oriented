1bfab41c055d752dbb63c64e0099d4ac6cb66935
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-09-27 10:16:33 -0700

    added #ace_adjusted_value

--- twenty_one.rb
+++ twenty_one.rb
@@ -1,5 +1,32 @@
+module Hand
+  attr_reader :hand
+
+  def display_hand
+    puts "#{name} has these cards:"
+    hand.each { |card| puts card.to_s }
+    puts "Total value of: #{hand_value}"
+    puts
+  end
+
+  def hand_value
+    aces = hand.count { |card| card.name == 'ACE'}
+    value = hand.map(&:value).reduce(:+)
+    return value if aces == 0
+    ace_adjusted_value(value, aces) if (value > 21) && (aces > 0)
+  end
+
+  private
+
+  def ace_adjusted_value(value, num_of_aces)
+    return value if (num_of_aces == 0) || (value <= 21)
+    ace_adjusted_value(value - 10, num_of_aces - 1)
+  end
+end
+
 class Player
-  attr_reader :name, :hand
+  include Hand
+
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -10,25 +37,19 @@ class Player
     hand << card
   end
 
-  def display_hand
-    puts "#{name} has these cards:"
-    hand.each { |card| puts card.to_s }
-    puts
-  end
-
   def busted?
     hand_value > 21
-  end
-
-  private
-
-  def hand_value
-    hand.map(&:value).reduce(:+)
   end
 end
 
 class Dealer < Player
   MAX_VALUE = 17
+
+  def hit(card)
+    puts "#{name} is drawing a card..."
+    sleep 2
+    super
+  end
 
   def stay?
     hand_value >= MAX_VALUE
@@ -96,10 +117,8 @@ class Game
     deal_cards
     display_table
     player_turn
-    display_table
     dealer_turn
-    display_table
-    # show_result
+    display_results
   end
 
   private
@@ -112,8 +131,20 @@ class Game
 
   def welcome_message
     clear
-    puts "Welcome to Twenty-One!"
+    puts '         Welcome to Twenty-One!'
+    puts '-----------------------------------------'
     puts
+    puts "You'll be dealt two cards to start."
+    puts
+    puts 'Request additional cards from the dealer'
+    puts "until you're happy with your hand, or"
+    puts 'you bust.'
+    puts
+    puts "Compare your hand against the dealer's"
+    puts 'hand to see who wins!'
+    puts
+    print 'Press RETURN to continue...'
+    gets
   end
 
   def deal_cards
@@ -133,21 +164,37 @@ class Game
       break if player.busted?
       display_table
     end
+    display_table
   end
 
   def dealer_turn
     loop do
       break if player.busted?
-      dealer.busted? || dealer.stay? ? break : dealer.hit(deck.deal_card)
+      (dealer.busted? || dealer.stay?) ? break : dealer.hit(deck.deal_card)
       display_table
       sleep 1
     end
+    display_table
   end
 
   def hit?
     print 'Would you like another card? (y/n): '
     choice = gets.chomp.downcase
     choice == 'y'
+  end
+
+  def display_results
+    if player.busted?
+      puts 'Sorry, you busted! Dealer wins the round.'
+    elsif dealer.busted?
+      puts 'Dealer busted. You win the round!'
+    elsif player.hand_value > dealer.hand_value
+      puts 'You have the best hand. You win the round!'
+    elsif dealer.hand_value > player.hand_value
+      puts 'Dealer has the best hand. Dealer wins the round.'
+    elsif dealer.hand_value == player.hand_value
+      puts "It's a tie! No one wins the round."
+    end
   end
 end
 
