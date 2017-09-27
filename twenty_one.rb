3867bfae1ecc3b17fc23688406346ebe76c34539
Jason Slusarchuk <jason.slusarchuk@gmail.com>
2017-09-27 07:30:59 -0700

    added Card and Deck class implementation

--- twenty_one.rb
+++ twenty_one.rb
@@ -45,30 +45,66 @@ class Participant
 end
 
 class Deck
+  SUITS = %w[HEARTS DIAMONDS SPADES CLUBS]
+  NAMES = %w[2 3 4 5 6 7 8 9 10 JACK QUEEN KING ACE]
+
+  attr_reader :cards
+
   def initialize
-    # obviously, we need some data structure to keep track of cards
-    # array, hash, something else?
+    @cards = new_deck_of_cards
   end
 
-  def deal
-    # does the dealer or the deck deal?
+  def new_deck_of_cards
+    SUITS.each_with_object([]) do |suit, cards|
+      NAMES.each do |name|
+        value = name.to_i
+        value = Card::FACE_VALUE if value == 0
+        value = Card::ACE_VALUE if name == 'ACE'
+        cards << Card.new(name, suit, value)
+      end
+    end
+  end
+
+  def deal_card
+
   end
 end
 
 class Card
-  def initialize
-    # what are the "states" of a card?
+  FACE_VALUE = 10
+  ACE_VALUE = 11
+
+  attr_reader :name, :suit, :value
+
+  def initialize(name, suit, value)
+    @name = name
+    @suit = suit
+    @value = value
+  end
+
+  def to_s
+    "#{name} of #{suit}"
   end
 end
 
 class Game
-  def start
-    deal_cards
-    show_initial_cards
-    player_turn
-    dealer_turn
-    show_result
+  def initialize
+    @deck
+    @player
+    @dealer
   end
+
+  def start
+    Deck.new.cards.each {|card| puts card.to_s}
+    # deal_cards
+    # show_initial_cards
+    # player_turn
+    # dealer_turn
+    # show_result
+  end
+
+  private
+
 end
 
 Game.new.start
